347d65045108f90abd4abe53c5e326099f709b26
Mauro <mauro@insided.nl>
2015-09-28 21:27:08 +0200

    Fix startup, not looping and prepare to metalsmith

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node
-var spawn = require('child_process').spawn,
-  //matchbox-window-manager -use_titlebar no -use_cursor no &
-  chrome = spawn('sh', ['./utils/chrome_start.sh'], {
-    cwd: __dirname
+var io = require('socket.io')(8080);
+
+// Here connection is created
+io.on('connection', function( socket ){
+  // Just emit a message
+  socket.emit('news', {hello: 'world!'});
+
+  // Just listen to some other events.
+  socket.on('mediacenter', function(data){
+      var spawn = require('child_process').spawn,
+        //matchbox-window-manager -use_titlebar no -use_cursor no &
+        chrome = spawn('sh', ['./utils/chrome_start.sh'], {
+          cwd: __dirname
+        });
+
+      chrome.stdout.on('data', function(data) {
+        console.log('stdout: ' + data);
+      });
+
+      chrome.stderr.on('data', function(data) {
+        console.log('stderr: ' + data);
+      });
+
+      chrome.on('close', function(code) {
+        console.log('child process exited with code ' + code);
+      });
   });
-
-chrome.stdout.on('data', function(data) {
-  console.log('stdout: ' + data);
-});
-
-chrome.stderr.on('data', function(data) {
-  console.log('stderr: ' + data);
-});
-
-chrome.on('close', function(code) {
-  console.log('child process exited with code ' + code);
 });
