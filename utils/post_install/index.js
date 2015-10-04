9885efd80b3409b5bf5dd0f717754477bf3f02fd
Mauro Mandracchia <info@ideabile.com>
2015-10-04 21:49:41 +0000

    Fix for the post_install

--- utils/post_install/index.js
+++ utils/post_install/index.js
@@ -4,7 +4,7 @@
 var jsonfile = require('jsonfile')
 
 // Change Virtual GamePad port
-var file = 'node_modules/virtual-gamepads/config.json',
+var file = '../virtual-gamepads/config.json',
     obj = {port: 3034};
 jsonfile.writeFileSync(file, obj, {spaces: 2});
 console.log('Virtal Gamepad Config on port: '+obj.port+' [ OK ]');
